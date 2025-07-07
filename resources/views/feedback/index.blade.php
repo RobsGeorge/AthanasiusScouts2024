02131366a26b0bbbab2f969d13c70ab61ca66998
Robeir George <robier_samir@hotmail.co.uk>
2025-07-07 23:08:35 +0300

    Adding Shamandora Logo to Feedback Page

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
-<html lang="ar" dir="rtl">
+<html lang="ar">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>تقييم معسكر مجمع 2025</title>
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>كشافة الشمندورة - تقييم معسكر مجمع 2025</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+        <style>
+  @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;500&display=swap');
+    </style>
+    <link rel="icon" type="image/x-icon" href={{ asset('img/shamandora.png') }}>
+    <!-- Custom styles for this template-->
+    <link href="../css/sb-admin-2.css" rel="stylesheet">
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Custom styles for this page -->
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    
+</head>
     <style>
         * {
             box-sizing: border-box;
@@ -287,7 +309,7 @@
 <body>
     <div class="form-container">
         <div class="form-header">
-            <img src="shamandora.png">
+            <img src="asset('img/shamandora.png')">
             <h1>تقييم معسكر مجمع 2025 - مربوط بكيفي</h1>
             <p>ربنا يعوضكم على تعبكم وخدمتكم خلال المعسكر.</p>
             <p>وجودكم كان بركة حقيقية، ومجهودكم هو اللي صنع الفرق.</p>
