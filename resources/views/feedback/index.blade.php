fe9d8daa5f7b794a96a5b61599bb681384d87520
Robeir George <robier_samir@hotmail.co.uk>
2025-07-09 02:02:37 +0300

    Add Shamandora icon to feedback page head

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href={{ asset('img/shamandora.png') }}>
     <title>تقييم معسكر مجمع 2025</title>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap" rel="stylesheet">
     <style>
